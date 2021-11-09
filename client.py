17d20f564645000412d7176d07531f0652a99bd7
Borik Beglaryan <borik.beglaryan@goya.am>
2021-11-09 18:54:08 +0400

    all

--- client.py
+++ client.py
@@ -1,28 +1,66 @@
 import asyncio
 import websockets
-from aioconsole import aprint, ainput
+from aioconsole import ainput, aprint
 from rich import print as pr
+import time
+
+
+chats = None
+chat = None
+
+async def get_chats(websocket):
+    all_clients = await websocket.recv()
+    while True:
+        client = all_clients.split(',  ')
+        print(all_clients)
+        name = input('which chat  ')
+        if name in client:
+            print('ok')
+            break
+    return name
 
 
 async def get_sms(websocket):
+    global chat, chats
     sms = await websocket.recv()
-    pr(sms)
-    await get_sms(websocket)
+    if 'CHATS__!@#!#' not in sms:
+        pr(sms)
+        await get_sms(websocket)
+    else:
+        chats = sms[12::]
+
 
 
 async def send_sms(websocket):
+    global chat, chats
     sms = await ainput('')
-    await websocket.send(sms)
-    await send_sms(websocket)
-
+    if sms != 'CHANGE GROUP':
+        await websocket.send(sms + ' TO - > ' + chat)
+        await send_sms(websocket)
+    else:
+        await websocket.send('GET_ALL_CHATS')
+        while True:
+            if chats is not None:
+                print(chats)
+                break
+            await asyncio.sleep(0)
+        while True:
+            k = await ainput(' wich chat      ')
+            if k in chats:
+                chat = k
+                break
+        await send_sms(websocket)
 
 
 
 async def listener():
-    async with websockets.connect('ws://localhost:8796') as websocket:
+    global chat, chats
+    async with websockets.connect('ws://localhost:15346') as websocket:
         message = await websocket.recv()
-        nickname = input(message)
+        nickname = input(message + '  ')
         await websocket.send(nickname)
+        chat = await get_chats(websocket)
+
         task1 = asyncio.create_task(get_sms(websocket))
         task2 = asyncio.create_task(send_sms(websocket))
         await task1
--- server.py
+++ server.py
@@ -14,14 +14,29 @@ async def get_nickname(websocket):
 
 
 async def send_sms(websocket, sms):
-    for socket in clients:
-        if socket != websocket:
-            await socket.send(f'[red]{clients[websocket]}[/red]  ->  {sms}')
+    sms, chat = sms[:sms.index(' TO - > '):], sms[sms.index(' TO - > '):]
+    print('>'+chat[8::]+'<')
+    if chat[8::] == 'GROUP':
+        for socket in clients:
+            if socket != websocket:
+                await socket.send(f'[red]{clients[websocket]}[/red]  ->  {sms}')
+    else:
+        for socket in clients:
+            if clients[socket] == chat[8::]:
+                await socket.send(f'[red]{clients[websocket]} privat[/red]  ->  {sms}')
 
 
 async def get_sms(websocket):
     sms = await websocket.recv()
-    if sms != '':
+    print(sms)
+    if sms == '':
+        pass
+    elif sms == 'GET_ALL_CHATS':
+        print(1)
+        print('CHATS__!@#!#'+',  '.join(list(clients.values()) + ['GROUP']))
+        await websocket.send('CHATS__!@#!#'+',  '.join(list(clients.values()) + ['GROUP']))
+        print(1)
+    else:
         await send_sms(websocket, sms)
 
     await get_sms(websocket)
@@ -31,7 +46,9 @@ async def main_2(websocket, path):
     try:
         await send_nickname(websocket, 'nickname')
         nickname = await get_nickname(websocket)
-        print(nickname)
+        # print(nickname)
+        print(', '.join(clients.values()))
+        await websocket.send(',  '.join(list(clients.values()) + ['GROUP']))
         clients[websocket] = nickname
         # await send_sms(websocket, clients[websocket])
         task1 = asyncio.create_task(get_sms(websocket))
@@ -41,7 +58,7 @@ async def main_2(websocket, path):
 
 
 async def main():
-    async with websockets.serve(main_2, 'localhost', 8796):
+    async with websockets.serve(main_2, 'localhost', 15346):
         await asyncio.Future()
 
 
